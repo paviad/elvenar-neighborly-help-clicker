eb91d0f8cd6af1ed8760913cb7d16b24d8b0c85a
Aviad Pineles <paviad2@gmail.com>
2023-09-12 09:59:54 +0300

    reorganized

--- elv.py
+++ elv.py
@@ -1,4 +1,68 @@
+import os
 import pyautogui
+
+
+def img(path):
+    return os.path.join('images', path)
+
+
+# find a neighborly help button (either normal or golden), and click it
+def clickHelpButton(buttonTop):
+    greenLocation = pyautogui.locateOnScreen(img("help.bmp"), region=(0, buttonTop, 1920, 1100))
+    goldLocation = pyautogui.locateOnScreen(img("gold.bmp"), region=(0, buttonTop, 1920, 1100))
+
+    if not greenLocation and not goldLocation:
+        return (False, None)
+
+    greenTop = greenLocation.top if greenLocation else 10000
+    goldTop = goldLocation.top if goldLocation else 10000
+
+    helpLocation = greenLocation if greenTop < goldTop else goldLocation
+
+    buttonTop = helpLocation.top + 1
+    buttonLeft = helpLocation.left
+
+    print(f"Found at {buttonTop}")
+
+    pyautogui.click(buttonLeft + 1, buttonTop + 1)
+    pyautogui.sleep(0.5)
+
+    return (True, buttonTop)
+
+
+# find contribute targets in this order of priority: builder -> culture -> main hall
+def clickContribButton():
+    mainhallLocation = pyautogui.locateOnScreen(img("mainhall.bmp"))
+    bldr1Location = pyautogui.locateOnScreen(img("bldr1.bmp"))
+    bldr2Location = pyautogui.locateOnScreen(img("bldr2.bmp"))
+    cultureLocation = pyautogui.locateOnScreen(img("culture.bmp"))
+
+    contribLoc = bldr1Location or bldr2Location or cultureLocation or mainhallLocation
+
+    pyautogui.click(contribLoc.left, contribLoc.top)
+    pyautogui.sleep(0.7)
+
+    # check if already helped, if so, just contribute to main hall instead (always possible)
+    alreadyHelpedLocation = pyautogui.locateOnScreen(img("alreadyhelped.bmp"))
+    if alreadyHelpedLocation:
+        pyautogui.click(
+            alreadyHelpedLocation.left + alreadyHelpedLocation.width / 2,
+            alreadyHelpedLocation.top + alreadyHelpedLocation.height - 5,
+        )
+        pyautogui.sleep(0.5)
+        pyautogui.click(mainhallLocation.left, mainhallLocation.top)
+        pyautogui.sleep(0.7)
+
+
+# check if some reward is available
+def checkCollectReward():
+    rewardLocation = pyautogui.locateOnScreen(img("reward.bmp"))
+    if rewardLocation:
+        pyautogui.click(
+            rewardLocation.left + 10,
+            rewardLocation.top + rewardLocation.height - 10,
+        )
+        pyautogui.sleep(0.5)
 
 
 def main():
@@ -7,60 +71,16 @@ def main():
         buttonTop = 0
         keepLooking = True
         while keepLooking:
-            # find a neighborly help button (either normal or golden), and click it
-            greenLocation = pyautogui.locateOnScreen("help.bmp", region=(0, buttonTop, 1920, 1100))
-            goldLocation = pyautogui.locateOnScreen("gold.bmp", region=(0, buttonTop, 1920, 1100))
-
-            if not greenLocation and not goldLocation:
-                keepLooking = False
+            (keepLooking, buttonTop) = clickHelpButton(buttonTop)
+            if not keepLooking:
                 break
 
-            greenTop = greenLocation.top if greenLocation else 10000
-            goldTop = goldLocation.top if goldLocation else 10000
+            clickContribButton()
 
-            helpLocation = greenLocation if greenTop < goldTop else goldLocation
+            checkCollectReward()
 
-            buttonTop = helpLocation.top + 1
-            buttonLeft = helpLocation.left
-
-            print(f"Found at {buttonTop}")
-
-            pyautogui.click(buttonLeft + 1, buttonTop + 1)
-            pyautogui.sleep(0.5)
-
-            mainhallLocation = pyautogui.locateOnScreen("mainhall.bmp")
-            bldr1Location = pyautogui.locateOnScreen("bldr1.bmp")
-            bldr2Location = pyautogui.locateOnScreen("bldr2.bmp")
-            cultureLocation = pyautogui.locateOnScreen("culture.bmp")
-
-            contribLoc = bldr1Location or bldr2Location or cultureLocation or mainhallLocation
-
-            pyautogui.click(contribLoc.left, contribLoc.top)
-
-            pyautogui.sleep(0.7)
-
-            # check if some reward is available
-            rewardLocation = pyautogui.locateOnScreen("reward.bmp")
-            if rewardLocation:
-                pyautogui.click(
-                    rewardLocation.left + 10,
-                    rewardLocation.top + rewardLocation.height - 10,
-                )
-                pyautogui.sleep(0.5)
-
-            # check if already helper
-            alreadyHelpedLocation = pyautogui.locateOnScreen("alreadyhelped.bmp")
-            if alreadyHelpedLocation:
-                pyautogui.click(
-                    alreadyHelpedLocation.left + alreadyHelpedLocation.width / 2,
-                    alreadyHelpedLocation.top + alreadyHelpedLocation.height - 5,
-                )
-                pyautogui.sleep(0.5)
-                pyautogui.click(mainhallLocation.left, mainhallLocation.top)
-                pyautogui.sleep(0.7)
-
-        nextPageLocation = pyautogui.locateOnScreen("nextpage.bmp")
-        lastPageLocation = pyautogui.locateOnScreen("lastpage.bmp")
+        nextPageLocation = pyautogui.locateOnScreen(img("nextpage.bmp"))
+        lastPageLocation = pyautogui.locateOnScreen(img("lastpage.bmp"))
 
         if lastPageLocation or not nextPageLocation:
             keepPaging = False
